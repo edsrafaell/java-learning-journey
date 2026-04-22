e283d1d084a67a71546931407ad105af07607d92
EdsonRafaelS <eds.rrafael@gmail.com>
2026-04-21 23:40:59 -0300

    study : adiciona exemplos de fundamentos Java
    Classes, Imprimir e Tipos Primitivos para iniciar estudos da linguagem.

---
+++ src/fundamentos/TiposPrimitivos.java
@@ -0,0 +1,13 @@
+package fundamentos;
+
+public class TiposPrimitivos {
+    public static void main(String[] args){
+        int num1 = 100;
+        char sexo = 'M';
+        String nome = "Rafael";
+        System.out.println(num1);
+        System.out.println(sexo);
+        System.out.println(nome);
+
+    }
+}

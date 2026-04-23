115eb26937fbcec4498d8489cd7e51fcc114258d
EdsonRafaelS <eds.rrafael@gmail.com>
2026-04-22 23:35:00 -0300

    study : adiciona exemplos de dados de impressão
    Tipos de dados de impressão:
    println, print e concatenação.

---
+++ src/fundamentos/DadosImpressao.java
@@ -0,0 +1,24 @@
+package fundamentos;
+
+public class DadosImpressao {
+    public static void main(String[] args){
+        String name = "Rafael";
+        int age = 10;
+
+        //Print + nova linha
+        System.err.println("Nome: ");
+        System.err.println(name);
+        System.err.println("Age: ");
+        System.err.println(age);
+
+        // Print + mesma linha
+        System.err.print("Nome: ");
+        System.err.print(name);
+        System.err.print("Age: ");
+        System.err.print(age);
+
+        //Concatenação
+        System.err.println("Nome: " + name);
+        System.err.println("Age: " + age);
+    }
+}

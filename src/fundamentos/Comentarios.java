29f07dd31ea26397ba3517d8644ddc32befcc4fe
EdsonRafaelS <eds.rrafael@gmail.com>
2026-04-23 23:43:14 -0300

    Study : Tipos de comentarios
    Study: Comentario de linha, inline //;
    Comentario de multiplas linhas /**/;
    Comentario de documentação /** * */;

---
+++ src/fundamentos/Comentarios.java
@@ -0,0 +1,19 @@
+package fundamentos;
+
+public class Comentarios {
+    public static void main(String[] args) {
+        // Comentário de uma linha
+
+        /*
+         * Comentário de múltiplas linhas
+         * Comentário de múltiplas linhas
+         * Comentário de múltiplas linhas
+         */
+
+        /**
+         * Comentário de documentação
+         * Comentário de documentação
+         * Comentário de documentação
+         */
+    }
+}

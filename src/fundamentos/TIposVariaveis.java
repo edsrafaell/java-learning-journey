ef7a8aaafed248b18780401b99f1eab97f301900
EdsonRafaelS <eds.rrafael@gmail.com>
2026-04-23 23:41:21 -0300

    study : finalizado estudos sobre Tipos de Variaveis
    Study : INT, STRING, LONG, VAR, BOOLEAN ...

---
+++ src/fundamentos/TIposVariaveis.java
@@ -0,0 +1,32 @@
+package fundamentos;
+
+public class TIposVariaveis {
+    public static void main(String[] args) {
+        String dayOfWeek = "Moday";
+        System.out.println(dayOfWeek);
+
+        dayOfWeek = "Tuesday";
+        System.err.println(dayOfWeek);
+
+        //Variavel = Variavel
+        int num = 1;
+        int one = num;
+        System.out.println(one);
+
+        String Nome = "Fael", Sobrenome = "Rafael";
+        System.out.println(Nome);
+        System.out.println(Sobrenome);
+
+        int age; 
+        age = 18;
+        System.out.println(age);
+
+        var nome = "Fael";
+        var idade = 10;
+        System.out.println(nome + idade);
+
+
+        
+    }
+    
+}

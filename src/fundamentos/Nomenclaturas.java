d33acf2338f7c98f0e27afabdace368e020630c8
EdsonRafaelS <eds.rrafael@gmail.com>
2026-04-25 00:21:54 -0300

    Study : Nomenclaturas e Variaveis
    Estudos de como nomear uma variavel

---
+++ src/fundamentos/Nomenclaturas.java
@@ -0,0 +1,15 @@
+package fundamentos;
+public class Nomenclaturas {
+    public static void main(String[] args) {
+        //Nomes de variaveis
+        int idade = 30; //Forma correta de nomear variaveis
+        int i = 20; //Forma errada de nomear variaveis
+
+        String nomeCompleto = "Rafael Santana"; //Forma correta de nomear variaveis
+        String nc = "Rafael Santana"; //Forma errada de nomear variaveis
+        String nomecompleto = "Rafael Santana"; //Forma errada de nomear variaveis
+        String $nomeCompleto = "Rafael Santana"; //Forma correta de nomear variaveis
+
+        
+    }
+}

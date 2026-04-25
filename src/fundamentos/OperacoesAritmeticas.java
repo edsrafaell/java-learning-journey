a155c1bd4d3e7753ab64552e8a5ec50ee65ed5b5
EdsonRafaelS <eds.rrafael@gmail.com>
2026-04-25 00:27:59 -0300

    Study : Operações Aritmeticas
    Estudos sobre Operações Aritméticas
    Adição +
    Subtração -
    Multiplicação *
    Divisão /
    Resto da Divisão %

---
+++ src/fundamentos/OperacoesAritmeticas.java
@@ -0,0 +1,21 @@
+package fundamentos;
+
+public class OperacoesAritmeticas {
+    public static void main(String[] args) {
+        System.out.println(2 + 3); // Adição
+        System.out.println(2 - 3); // Subtração
+        System.out.println(2 * 3); // Multiplicação
+        System.out.println(2 / 3); // Divisão
+        System.out.println(2 % 3); // Resto da divisão
+
+        int a = 10;
+        int b = 20;
+
+        System.out.println(a + b); // Adição
+        System.out.println(a - b); // Subtração
+        System.out.println(a * b); // Multiplicação
+        System.out.println(a / b); // Divisão
+        System.out.println(a % b); // Resto da divisão
+
+    }
+}

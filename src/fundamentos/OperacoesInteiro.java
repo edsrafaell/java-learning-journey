678ed07768515254dc8ffc1b57f41cdb46e4f34c
EdsonRafaelS <eds.rrafael@gmail.com>
2026-04-25 23:49:41 -0300

    Study : Incremento e Decremento
    Estudos sobre operações de incremento e decremento
    ++ e --

--- src/fundamentos/OperacoesInteiro.java
+++ src/fundamentos/OperacoesInteiro.java
@@ -20,5 +20,16 @@ public class OperacoesInteiro {
 
         sum *= a;
         System.out.println(sum);
+
+        
+        //inclemento e declemento
+        int as = 10;
+        int bs = ++as; // Incremento pré-fixado: a é incrementado antes de ser atribuído a b
+        System.out.println("as: " + as); // as: 11
+        System.out.println("bs: " + bs); // bs: 11
+
+        int bs2 = as++; // Incremento pós-fixado: a é atribuído a b antes de ser incrementado
+        System.out.println("as: " + as); // as: 12
+        System.out.println("bs2: " + bs2); // bs2: 11
     }
 }

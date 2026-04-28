249021b6ab2d8915f546424ee7a9f405bc261726
EdsonRafaelS <eds.rrafael@gmail.com>
2026-04-27 23:02:02 -0300

    Update

---
+++ src/fundamentos/OperacoesAritmeticas.java
@@ -0,0 +1,24 @@
+package fundamentos;
+
+public class OperacoesAritmeticas {
+    public static void main(String[] args) {
+        System.out.println(2 + 3); // Adição
+        System.out.println(2 - 3); // Subtração
+        System.out.println(2 * 3); // Multiplicação
+        System.out.println(2 / 3); // Divisão
+        System.out.println(2 % 3); // Resto da divisão
+
+        int a = 10;
+        int b = 20;
+
+        System.out.println(a + b); // Adição
+        System.out.println(a - b); // Subtração
+        System.out.println(a * b); // Multiplicação
+        System.out.println(a / b); // Divisão
+        System.out.println(a % b); // Resto da divisão
+
+        //Ordem de precedência
+        System.out.println(((2 + 3) * 4) / 2); // Parênteses, multiplicação e divisão, adição e subtração
+        System.out.println(2 + 3 * 4 / 2); // Multiplicação e divisão, adição e subtração
+    }
+}
